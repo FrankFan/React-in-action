46bffa3d1bcc6b574e6ad1089b75a4453e4584dd
FrankFan <fanyong@gmail.com>
2015-11-09 23:30:45 +0800

    add radioOptionGroup component

--- radioGroupDemo/components/Demo.jsx
+++ radioGroupDemo/components/Demo.jsx
@@ -1,5 +1,13 @@
 var Demo = React.createClass({
 	render: function () {
+		var radioOptions = [
+			{ value: 'newspaper', label: 'Newspaper'},
+			{ value: 'radio', label: 'Radio'},
+			{ value: 'tv', label: 'Television'},
+			{ value: 'search', label: 'Search Engine'},
+			{ value: 'social', label: 'Social Media'}
+		];
+
 		return (
 			<div className="container">
 				<h1>React.js Radio Group Demo</h1>
@@ -7,27 +15,7 @@ var Demo = React.createClass({
 				<form>
 					<p className="h3">How did you hear about us?</p>
 
-					<RadioOption value="newspaper">
-						Newspaper
-					</RadioOption>
-
-					<RadioOption value="radio">
-						Radio
-					</RadioOption>
-
-					<RadioOption value="tv">
-						Television
-					</RadioOption>
-
-					<RadioOption value="search">
-						Search Engine
-					</RadioOption>
-
-					<RadioOption value="social">
-						Social Media
-					</RadioOption>
-
-					<RadioOtherOption />
+					<RadioOptionGroup options={radioOptions} />
 
 					<p><input type="submit"/></p>
 				</form>
