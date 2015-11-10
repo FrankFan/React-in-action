984df4245fca2a1f9ed6dc7bff69557daf80db7a
fanyong <fanyong@gmail.com>
2015-11-10 11:08:30 +0800

    update demo2

--- demo2/index.jsx
+++ demo2/index.jsx
@@ -1,8 +1,8 @@
 /** @jsx React.DOM */
 'use strict'
-var React = require('react')
-var ReactDOM = require('react-dom')
-var Hello = require('./Hello')
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Hello = require('./Hello.jsx');
 
-// console.log(Hello);
-// ReactDOM.render(<Hello /> , document.getElementById('content'));
+console.log(Hello);
+ReactDOM.render(<Hello /> , document.getElementById('content'));
--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -1,32 +1,30 @@
 /* webpack config file */
-// 失败了，总是报错“ Cannot resolve 'file' or 'directory' ./Hello”
-// 不知道哪里配置的不对
-
 
 module.exports = {
-	entry: './index.jsx',
-	output: {
-		path: './dist',
-		filename: 'bundle.js', // 默认为 bundle.js
-		publicPath: 'http://localhost:8090/assets'
-	},
-	module: {
-		loaders: [
-			{
-				// tell webpack to use jsx-loader for all *.jsx files
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-				// loader: 'jsx-loader?insertPragma=React.DOM&harmony'
-			}
-		]
-	},
-	externals: {
-		// don't bundle the 'react' npm package with our bundle.js
-		// but get it from a global 'React' variable
-		'react': 'React'
-	},
-	resolve: {
-		extentsions: ['', '.js', '.jsx']
-	}
+    entry: './index.jsx',
+    output: {
+    	path: './dist/',
+        filename: 'bundle.js', //this is the default name, so you can skip it
+        //at this directory our bundle file will be available
+        //make sure port 8090 is used when launching webpack-dev-server
+        publicPath: 'http://localhost:8090/assets'
+    },
+    module: {
+        loaders: [
+            {
+                //tell webpack to use jsx-loader for all *.jsx files
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+            }
+        ]
+    },
+    externals: {
+        //don't bundle the 'react' npm package with our bundle.js
+        //but get it from a global 'React' variable
+        'react': 'React'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
 }
