8fc854129fe1121a9f892b122567e14d032e91ef
FrankFan <fanyong@gmail.com>
2015-11-14 22:57:47 +0800

    add react-dom dep

--- es6-react-babel-webpack/hello.jsx
+++ es6-react-babel-webpack/hello.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
  
 class Hello extends React.Component {
   render() {
-    return <h1>Hello</h1>
+    return <h1>Hello 1</h1>
   }
 }
 
-React.render(<Hello/>, document.getElementById('hello'));
+ReactDOM.render(<Hello/>, document.getElementById('hello'));
