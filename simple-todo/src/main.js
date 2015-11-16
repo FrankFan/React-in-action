54954a446faf957acb1ba8fbb33584e278b97de8
fanyong <fanyong@gmail.com>
2015-11-16 17:33:58 +0800

    add style loader

--- simple-todo/src/main.js
+++ simple-todo/src/main.js
@@ -3,6 +3,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var TodoApp = require('./components/TodoApp.jsx');
+require("./style/main.css");
 
 var mountNode = document.getElementById('app');
 ReactDOM.render(<TodoApp />, mountNode);
